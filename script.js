7ce80e23d3bc4def892e75c0da0385b88644754f
Bibi Hagenow <bibi_hagenow@hotmail.com>
2019-02-12 13:58:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function colorPicker(event) {}
+
+function convertHEXtoHSL() {}
+
+function setBaseColor() {}
+
+function calculateColors(base) {}
+
+function analogous() {}
+
+function monochromatic() {}
+
+function traid() {}
+
+function complementary() {}
+
+function compound() {}
+
+function shades() {}
+
+function setColor(id, color) {}
